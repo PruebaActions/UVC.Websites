8cade746d9a6bc4f65671db66d4ad301b69583e1
Frank <frank.reyes@solvex.com.do>
2025-12-21 01:20:56 -0400

    Update pull request workflow and modify HomeController and Index view content

--- DescopeSampleApp/Controllers/HomeController.cs
+++ DescopeSampleApp/Controllers/HomeController.cs
@@ -8,7 +8,7 @@ namespace DescopeSampleApp.Controllers
 {
     public class HomeController : Controller
     {
-    //zcxsadsadsasdasadcxsadcxzds
+    //asdcxzcxzweawd
         public ActionResult Index()
         {
             ViewBag.Title = "Home Page";
